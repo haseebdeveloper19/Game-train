49e49fde07f0b1632801dd1bd5e0346fba453e63
haseeb developer <haseebdeveloper19@gmail.com>
2022-07-06 11:10:40 +0500

    form bootstrap

---
+++ asstes/js/data.js
@@ -0,0 +1,14 @@
+function data() {
+  var dom = document;
+  var fnam = dom.getElementById("fname");
+  var lnam = dom.getElementById("lname");
+  var emai = dom.getElementById("email");
+  var passwor = dom.getElementById("password");
+  var confirmPasswor = dom.getElementById("confirmPassword");
+  var a = fname.value;
+  console.log("First Name is", fnam.value);
+  console.log("Last Name is", lnam.value);
+  console.log("Email is", emai.value);
+  console.log("Password is", passwor.value);
+  console.log("confirm Password is", confirmPassword.value);
+}
